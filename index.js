997bcd59217bebde521684505c9e7901fc94de1a
Jeff Knaggs <mail@jeef3.com>
2014-01-30 10:13:31 +1300

    Some basic app set-up

---
+++ index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+var express = require('express');
+var app = express();
+
+app.post('/api/build-started', function (req, res) {
+  res.send('Hello World!');
+});
+
+var port = Number(process.env.PORT || 5000);
+app.listen(port, function() {
+  console.log('Listening on ' + port);
+});
