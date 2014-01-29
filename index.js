d0ffd14e6c415f08c2cce738b8aa43b9d01d9448
Jeff Knaggs <mail@jeef3.com>
2014-01-30 10:32:35 +1300

    Added Mongo

--- index.js
+++ index.js
@@ -1,10 +1,38 @@
 'use strict';
 
 var express = require('express');
+var mongo = require('mongoskin');
+
+var mongoUri = process.env.MONGOLAB_URI ||
+  process.env.MONGOHQ_URL ||
+  'mongodb://localhost/better-web-hooks';
+
 var app = express();
 
-app.post('/api/build-started', function (req, res) {
-  res.send('Hello World!');
+var eventsCollection = function () {
+  return mongo.db(mongoUri, {safe:true}).collection('events');
+};
+
+app.post('/api/events', function (req, res) {
+  eventsCollection().insert(event, { strict: true }, function (err, doc) {
+    if (err) {
+      res.send(500);
+      return;
+    }
+
+    res.send(doc);
+  });
+});
+
+app.get('/api/events', function (req, res) {
+  eventsCollection().find({}, function (err, cursor) {
+    if (err) {
+      res.send(500);
+      return;
+    }
+
+    res.send(cursor);
+  });
 });
 
 var port = Number(process.env.PORT || 5000);
